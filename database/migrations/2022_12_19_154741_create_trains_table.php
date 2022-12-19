96a32340c599d3d42ff6cb6588fd0b41a59ef698
CarmineC93 <107880239+CarmineC93@users.noreply.github.com>
2022-12-19 17:16:54 +0100

    migraite failed

---
+++ database/migrations/2022_12_19_154741_create_trains_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trains', function (Blueprint $table) {
+            $table->id();
+            $table->string('company');
+            $table->string('start_station');
+            $table->string('end_station');
+            $table->time('departure_time');
+            $table->time('arrival_time');
+            $table->integer('train_code', 10);
+            $table->tinyInteger('carriage_nmbr');
+            $table->tinyInteger("in_time");
+            $table->tinyInteger('deleted')->unsigned()->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trains');
+    }
+};
--- routes/web.php
+++ routes/web.php
@@ -13,4 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [PageController::class, 'index'])->name('home');
+Route::get(
+    '/',
+    //[PageController::class, 'index']
+)->name('home');
