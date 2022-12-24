782f9d3ee6a7307873385d8b414fdf11f19ece31
CarmineC93 <107880239+CarmineC93@users.noreply.github.com>
2022-12-24 15:58:15 +0100

    file CSV seeder completed

--- database/seeders/TrainsTableSeederCSV.php
+++ database/seeders/TrainsTableSeederCSV.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Functions\Helpers;
+use App\Models\Train;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -16,6 +17,20 @@ class TrainsTableSeederCSV extends Seeder
     public function run()
     {
         $trainsData = Helpers::getCsvData(__DIR__ . '/trains.csv');
-        dd($trainsData);
+        foreach ($trainsData as $key => $train) {
+            if ($key !== 0) {
+                $newTrain = new Train();
+                $newTrain->company = $train[0];
+                $newTrain->start_station = $train[1];
+                $newTrain->end_station = $train[2];
+                $newTrain->departure_time = $train[3];
+                $newTrain->arrival_time = $train[4];
+                $newTrain->train_code = $train[5];
+                $newTrain->carriage_nmbr = $train[6];
+                $newTrain->in_time = $train[7];
+                $newTrain->deleted = $train[8];
+                $newTrain->save();
+            }
+        }
     }
 }
