ab39b4889124c4f10e529602496da73cc44cc60e
CarmineC93 <107880239+CarmineC93@users.noreply.github.com>
2022-12-24 15:42:57 +0100

    add file Functions with Helpers( get data from csv file)

--- database/seeders/TrainsTableSeeder.php
+++ database/seeders/TrainsTableSeeder.php
@@ -19,11 +19,11 @@ class TrainsTableSeeder extends Seeder
         for ($i = 0; $i < 5; $i++) {
             $newTrain = new Train();
             $newTrain->company = $faker->words(2, true);
-            $newTrain->start_station = $faker->words(2, true);
-            $newTrain->end_station = $faker->words(2, true);
-            $newTrain->departure_time = $faker->date('22_12_d');
-            $newTrain->arrival_time = $faker->dateTime('22_12_d_H_i_s');
-            $newTrain->train_code = $faker->word(1);
+            $newTrain->start_station = $faker->city();
+            $newTrain->end_station = $faker->city();
+            $newTrain->departure_time = $faker->dateTimeBetween('-1 week', '+1 week');
+            $newTrain->arrival_time = $faker->dateTimeInInterval($newTrain->departure_time, "+20 hour");
+            $newTrain->train_code = $faker->bothify('####');
             $newTrain->carriage_nmbr = $faker->numberBetween(1, 10);
             $newTrain->in_time = $faker->numberBetween(0, 1);
             $newTrain->deleted = $faker->numberBetween(0, 1);
